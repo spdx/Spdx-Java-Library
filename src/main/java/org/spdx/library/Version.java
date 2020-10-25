c03619d647739c63136c067a7ab9b309f8fb8ae0
Gary O'Neall <gary@sourceauditor.com>
2020-10-25 14:17:20 -0700

    Allow matches to (c) and copyright symbol in license matcher
    Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

--- src/main/java/org/spdx/library/Version.java
+++ src/main/java/org/spdx/library/Version.java
@@ -46,7 +46,7 @@ public class Version {
 			TWO_POINT_ONE_VERSION, TWO_POINT_TWO_VERSION
 	})));
 	
-	public static final String CURRENT_IMPLEMENTATION_VERSION = "0.3";
+	public static final String CURRENT_IMPLEMENTATION_VERSION = "0.4";
 	
 	public static String verifySpdxVersion(String spdxVersion) {
 		if (!spdxVersion.startsWith("SPDX-")) {
