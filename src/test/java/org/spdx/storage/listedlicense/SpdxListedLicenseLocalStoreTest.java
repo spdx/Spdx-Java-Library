34b64d618a9cf98d61019484c251dac0ed566569
Gary O'Neall <gary@sourceauditor.com>
2022-06-24 15:29:46 -0700

    Update license cache to verion 3.17
    Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

--- src/test/java/org/spdx/storage/listedlicense/SpdxListedLicenseLocalStoreTest.java
+++ src/test/java/org/spdx/storage/listedlicense/SpdxListedLicenseLocalStoreTest.java
@@ -46,7 +46,7 @@ public class SpdxListedLicenseLocalStoreTest extends TestCase {
 
 	private static final String APACHE_ID = "Apache-2.0";
 	private static final String LICENSE_LIST_URI = "https://spdx.org/licenses/";
-	private static final String LICENSE_LIST_VERSION = "3.7";
+	private static final String LICENSE_LIST_VERSION = "3.17";
 	private static final String APACHE_LICENSE_NAME = "Apache License 2.0";
 	
 	private static final String ECOS_EXCEPTION_ID = "eCos-exception-2.0";
