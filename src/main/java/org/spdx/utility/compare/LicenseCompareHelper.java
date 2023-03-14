spdx/Spdx-Java-Library

commit cffcbf97060731c7d48a3624a6b3f2678fc7cb5c

Add 'var' text support for matching within text (#156)
* Add 'var' text support for matching within text

Fixes #155

Note: This change introduces a severe performance issue for some
licenses - noteably GPL3

Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

* Limit regexes in find license in text

Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

* Remove back to back match quantifiers

Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

* Improve license in text matching by removing comment chars

Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

* Add a 'slow test' environment variable for unit tests

Wraps all lenghty tests in a conditional which will only
run if the system environment variable
SPDX_JAVA_LIB_RUN_SLOW_TESTS=true

Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

---------

Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

src/main/java/org/spdx/utility/compare/LicenseCompareHelper.java
@@ -52,6 +52,7 @@ import org.spdx.licenseTemplate.LicenseParserException;
 import org.spdx.licenseTemplate.LicenseTemplateRuleException;
 import org.spdx.licenseTemplate.SpdxLicenseTemplateHelper;
 import org.spdx.utility.compare.CompareTemplateOutputHandler.DifferenceDescription;
+import org.spdx.utility.compare.FilterTemplateOutputHandler.VarTextHandling;
 
 /**
  * Primarily a static class of helper functions for comparing two SPDX licenses
@@ -75,6 +76,8 @@ public class LicenseCompareHelper {
 	protected static final Map<String, String> NORMALIZE_TOKENS = new HashMap<>();
 
 	protected static final Integer CROSS_REF_NUM_WORDS_MATCH = 80;
+	
+	protected static final Pattern REGEX_QUANTIFIER_PATTERN = Pattern.compile(".*\\.\\{(\\d+),(\\d+)\\}$");
 	
 	static {
 		//TODO: These should be moved to a property file
@@ -227,6 +230,7 @@ public class LicenseCompareHelper {
 	            	line = line.replaceAll("(\\*/|-->|-\\}|\\*\\))\\s*$", "");  // remove end of line comments
 	                line = line.replaceAll("^\\s*" + START_COMMENT_CHAR_PATTERN, "");  // remove start of line comments
                     line = line.replaceAll("^\\s*<<beginOptional>>\\s*" + START_COMMENT_CHAR_PATTERN, "<<beginOptional>>");
+                    line = line.replaceAll("(-|=|\\*){3,}", "");  // Remove ----, ***,  and ====
                     sb.append(line);
 	                sb.append("\n");
 	                line = reader.readLine();
@@ -631,8 +635,21 @@ public class LicenseCompareHelper {
 	 * @return list of strings for all non-optional license text.  
 	 * @throws SpdxCompareException
 	 */
+	@Deprecated
 	public static List<String> getNonOptionalLicenseText(String licenseTemplate, boolean includeVarText) throws SpdxCompareException {
-		FilterTemplateOutputHandler filteredOutput = new FilterTemplateOutputHandler(includeVarText);
+		return getNonOptionalLicenseText(licenseTemplate,
+				includeVarText ? VarTextHandling.ORIGINAL : VarTextHandling.OMIT);
+	}
+	
+	/**
+	 * Get the text of a license minus any optional text - note: this include the default variable text
+	 * @param licenseTemplate license template containing optional and var tags
+	 * @param varTextHandling include original, exclude, or include the regex (enclosed with "~~~") for "var" text
+	 * @return list of strings for all non-optional license text.  
+	 * @throws SpdxCompareException
+	 */
+	public static List<String> getNonOptionalLicenseText(String licenseTemplate, VarTextHandling varTextHandling) throws SpdxCompareException {
+		FilterTemplateOutputHandler filteredOutput = new FilterTemplateOutputHandler(varTextHandling);
 		try {
 			SpdxLicenseTemplateHelper.parseTemplate(licenseTemplate, filteredOutput);
 		} catch (LicenseTemplateRuleException e) {
@@ -657,25 +674,66 @@ public class LicenseCompareHelper {
 		int startTextIndex = 0;
 		int wordsInLastLine = 0;	// keep track of the number of words processed in the last start line to make sure we don't overlap words in the end lines
 		StringBuilder patternBuilder = new StringBuilder();
+		String regexLimit = "," + Integer.toString(numberOfWords * 10) + "}";
+		String lastRegex = "";
 		while (startWordCount < numberOfWords && startTextIndex < nonOptionalText.size()) {
-			String[] tokens = normalizeText(nonOptionalText.get(startTextIndex++).trim()).split("\\s");
-			int tokenIndex = 0;
-			wordsInLastLine = 0;
-			while (tokenIndex < tokens.length && startWordCount < numberOfWords) {
-				String token = tokens[tokenIndex++].trim();
-				if (token.length() > 0) {
-					if (NORMALIZE_TOKENS.containsKey(token.toLowerCase())) {
-						token = NORMALIZE_TOKENS.get(token.toLowerCase());
+			String line = nonOptionalText.get(startTextIndex++);
+			if (patternBuilder.length() > 0 && line.trim().length() > 0 && !patternBuilder.toString().endsWith("}")) {
+				patternBuilder.append(".{0,5}");
+			}
+			String[] regexSplits = line.trim().split(FilterTemplateOutputHandler.REGEX_ESCAPE);
+			boolean inRegex = false; // if it starts with a regex, it will start with a blank line
+			for (String regexSplit:regexSplits) {
+				if (inRegex && startWordCount < numberOfWords) {
+					String regexToAppend;
+					if (regexSplit.endsWith(".+")) {
+						regexToAppend = regexSplit.substring(0, regexSplit.length()-1) +"{1" + regexLimit;
+					} else if (regexSplit.endsWith(".*")) {
+						regexToAppend = regexSplit.substring(0, regexSplit.length()-1) +"{0" + regexLimit;
+					} else {
+						regexToAppend = regexSplit;
 					}
-					patternBuilder.append(Pattern.quote(token));
-					patternBuilder.append("\\s*");
+					if (patternBuilder.toString().endsWith("}") && regexToAppend.endsWith("}")) {
+						// collapse consecutive match anything
+						Matcher lastRegexMatch = REGEX_QUANTIFIER_PATTERN.matcher(lastRegex);
+						Matcher regexToAppendMatch = REGEX_QUANTIFIER_PATTERN.matcher(regexToAppend);
+						if (lastRegexMatch.matches() && regexToAppendMatch.matches()) {
+							int lastRegexMax = Integer.parseInt(lastRegexMatch.group(2));
+							int thisRegexMax = Integer.parseInt(regexToAppendMatch.group(2));
+							if (lastRegexMax >= thisRegexMax) {
+								regexToAppend = ""; // already covered by previous regex
+							} else {
+								// remove the last max
+								patternBuilder.setLength(patternBuilder.length()-(lastRegexMatch.group(2).length()+1));
+								regexToAppend = regexToAppend.substring(regexToAppend.indexOf(',')+1);
+							}
+						}
+					}
+					patternBuilder.append(regexToAppend);
+					lastRegex = regexToAppend;
 					startWordCount++;
-					wordsInLastLine++;
+					inRegex = false;
+				} else {
+					String[] tokens = normalizeText(regexSplit.trim()).split("\\s");
+					int tokenIndex = 0;
+					wordsInLastLine = 0;
+					while (tokenIndex < tokens.length && startWordCount < numberOfWords) {
+						String token = tokens[tokenIndex++].trim();
+						if (token.length() > 0) {
+							if (NORMALIZE_TOKENS.containsKey(token.toLowerCase())) {
+								token = NORMALIZE_TOKENS.get(token.toLowerCase());
+							}
+							patternBuilder.append(Pattern.quote(token));
+							patternBuilder.append("\\s*");
+							startWordCount++;
+							wordsInLastLine++;
+						}
+					}
+					inRegex = true;
 				}
 			}
-			patternBuilder.append(".*");
 		}
-		
+		patternBuilder.append(".{0,36000}");
 		// End words
 		List<String> endTextReversePattern = new ArrayList<>();
 		int endTextIndex = nonOptionalText.size()-1;
@@ -684,12 +742,26 @@ public class LicenseCompareHelper {
 		while (endWordCount < numberOfWords && 
 				(endTextIndex > lastProcessedStartLine || 
 						(endTextIndex == lastProcessedStartLine && (numberOfWords - endWordCount) < (nonOptionalText.get(endTextIndex).length() - wordsInLastLine)))) {	// Check to make sure we're not overlapping the start words
-			String[] tokens = normalizeText(nonOptionalText.get(endTextIndex).trim()).split("\\s");
 			List<String> nonEmptyTokens = new ArrayList<>();
-			for (String token:tokens) {
-				String trimmedToken = token.trim();
-				if (!trimmedToken.isEmpty()) {
-					nonEmptyTokens.add(trimmedToken);
+			String line = nonOptionalText.get(endTextIndex);
+			String[] regexSplits = line.trim().split(FilterTemplateOutputHandler.REGEX_ESCAPE);
+			boolean inRegex = false;
+			for (String regexSplit:regexSplits) {
+				if (inRegex) {
+					if (!regexSplit.isEmpty()) {
+						nonEmptyTokens.add(FilterTemplateOutputHandler.REGEX_ESCAPE + regexSplit);
+					}
+					inRegex = false;
+				} else {
+					String[] tokens = normalizeText(regexSplit.trim()).split("\\s");
+					
+					for (String token:tokens) {
+						String trimmedToken = token.trim();
+						if (!trimmedToken.isEmpty()) {
+							nonEmptyTokens.add(trimmedToken);
+						}
+					}
+					inRegex = true;
 				}
 			}
 			int remainingTokens = (endTextIndex == lastProcessedStartLine && nonEmptyTokens.size() - wordsInLastLine > numberOfWords - endWordCount) ? 
@@ -698,12 +770,15 @@ public class LicenseCompareHelper {
 			int tokenIndex = nonEmptyTokens.size() - 1;
 			while (tokenIndex >= 0 && remainingTokens > 0) {
 				String token = nonEmptyTokens.get(tokenIndex--);
-				endTextReversePattern.add("\\s*");
-				endTextReversePattern.add(Pattern.quote(token));
+				if (token.startsWith(FilterTemplateOutputHandler.REGEX_ESCAPE)) {
+					endTextReversePattern.add(token.substring(FilterTemplateOutputHandler.REGEX_ESCAPE.length()));
+				} else {
+					endTextReversePattern.add("\\s*");
+					endTextReversePattern.add(Pattern.quote(token));
+				}
 				remainingTokens--;
 				endWordCount++;
 			}
-			endTextReversePattern.add(".*");
 		}
 		
 		int revPatternIndex = endTextReversePattern.size()-1;
@@ -800,7 +875,6 @@ public class LicenseCompareHelper {
 						int normalizedPosition = result.length();
 						token = NORMALIZE_TOKENS.get(token.toLowerCase());
 						result.append(token);
-//						result.append(' ');
 						charPositions.add(new ImmutablePair<>(normalizedPosition, originalCurrentLinePosition + lineMatcher.start(1)));
 						charPositions.add(new ImmutablePair<>(result.length(), originalCurrentLinePosition + lineMatcher.end(1)));
 						lineCharPosition = lineMatcher.end(1);
@@ -862,10 +936,10 @@ public class LicenseCompareHelper {
 			return null;
 		}
 
-		List<String> templateNonOptionalText = getNonOptionalLicenseText(template, true);
+		List<String> templateNonOptionalText = getNonOptionalLicenseText(removeCommentChars(template), VarTextHandling.REGEX);
 		Pattern matchPattern = nonOptionalTextToStartPattern(templateNonOptionalText, CROSS_REF_NUM_WORDS_MATCH);
 		List<Pair<Integer, Integer>> charPositions = new ArrayList<>();
-		String normalizedText = normalizeText(text);
+		String normalizedText = removeCommentChars(normalizeText(text));
 		String compareText;
 		try {
 			compareText = normalizeTokensForRegex(normalizedText, charPositions);

src/test/java/org/spdx/utility/compare/LicenseCompareHelperTest.java
@@ -42,6 +42,7 @@ import org.spdx.library.model.license.SpdxListedLicense;
 import org.spdx.library.model.license.SpdxNoAssertionLicense;
 import org.spdx.library.model.license.SpdxNoneLicense;
 import org.spdx.utility.compare.CompareTemplateOutputHandler.DifferenceDescription;
+import org.spdx.utility.compare.FilterTemplateOutputHandler.VarTextHandling;
 
 import junit.framework.TestCase;
 
@@ -68,7 +69,9 @@ public class LicenseCompareHelperTest extends TestCase {
     static final String SGIB_1_0_TEXT = "TestFiles" + File.separator + "SGI-B-1.0.txt";
     static final String SGIB_1_0_TEMPLATE = "TestFiles" + File.separator + "SGI-B-1.0.template.txt";
     static final String APACHE_1_0_TEXT = "TestFiles" + File.separator + "Apache-1.0.txt";
-
+    static final String ATLASSAIN_BSD_FILE = "TestFiles" + File.separator + "atlassain-bsd";
+    static final String MPL_2_FROM_MOZILLA_FILE = "TestFiles" + File.separator + "mpl_2_from_mozilla.txt";
+    
 	/**
 	 * @throws java.lang.Exception
 	 */
@@ -628,14 +631,17 @@ public class LicenseCompareHelperTest extends TestCase {
 		assertFalse(result.isDifferenceFound());
 	}
 	
-//	public void testMatchingStandardLicenseIds() throws IOException, InvalidSPDXAnalysisException, SpdxCompareException {
-// This test is too slow!
-//		String compareText = UnitTestHelper.fileToText(GPL_2_TEXT);
-//		String[] result = LicenseCompareHelper.matchingStandardLicenseIds(compareText);
-//		assertEquals(2,result.length);
-//		assertTrue(result[0].startsWith("GPL-2"));
-//		assertTrue(result[1].startsWith("GPL-2"));
-//	}
+	public void testMatchingStandardLicenseIds() throws IOException, InvalidSPDXAnalysisException, SpdxCompareException {
+		if (UnitTestHelper.runSlowTests()) {
+			String compareText = UnitTestHelper.fileToText(GPL_2_TEXT);
+			String[] result = LicenseCompareHelper.matchingStandardLicenseIds(compareText);
+			assertEquals(4,result.length);
+			assertTrue(result[0].startsWith("GPL-2"));
+			assertTrue(result[1].startsWith("GPL-2"));
+			assertTrue(result[2].startsWith("GPL-2"));
+			assertTrue(result[3].startsWith("GPL-2"));
+		}
+	}
 	
 	public void testFirstLicenseToken() {
 		assertEquals("first", LicenseCompareHelper.getFirstLicenseToken("   first,token that is needed\nnext"));
@@ -723,6 +729,9 @@ public class LicenseCompareHelperTest extends TestCase {
 		assertTrue(LicenseCompareHelper.isStandardLicenseWithinText(multiLicenseText, apache20));
 		assertTrue(LicenseCompareHelper.isStandardLicenseWithinText(textWithRandomPrefixAndSuffix, apache20));
 		assertFalse(LicenseCompareHelper.isStandardLicenseWithinText(multiLicenseText, apache10));
+		String mplText = UnitTestHelper.fileToText(MPL_2_FROM_MOZILLA_FILE);
+		assertTrue(LicenseCompareHelper.isStandardLicenseWithinText(mplText, ListedLicenses.getListedLicenses().getListedLicenseById("MPL-2.0")));
+
 
 /* Currently doesn't work - see https://github.com/spdx/Spdx-Java-Library/issues/141 for details
 		// JavaMail license is "CDDL-1.1 OR GPL-2.0 WITH Classpath-exception-2.0"
@@ -735,6 +744,7 @@ public class LicenseCompareHelperTest extends TestCase {
 		assertFalse(LicenseCompareHelper.isStandardLicenseWithinText(javaMailLicense, apache20));
  */
 	}
+	
 	public void testIsStandardLicenseExceptionWithinText() throws InvalidSPDXAnalysisException, SpdxCompareException, IOException {
 		ListedLicenseException classpath20 = ListedLicenses.getListedLicenses().getListedExceptionById("Classpath-exception-2.0");
 		String gpl20 = ListedLicenses.getListedLicenses().getListedLicenseById("GPL-2.0").getLicenseText();
@@ -752,6 +762,7 @@ public class LicenseCompareHelperTest extends TestCase {
 	}
 
 	// Note: comparing lists directly in JUnit 4.x doesn't work as one might expect, so we use this helper
+	@SuppressWarnings({"rawtypes", "unchecked"})
 	private void assertListsEqual(List expected, List actual) {
 		if (expected == null) {
 			if (actual != null) {
@@ -771,24 +782,36 @@ public class LicenseCompareHelperTest extends TestCase {
 		String apache20 = ListedLicenses.getListedLicenses().getListedLicenseById("Apache-2.0").getLicenseText();
 		String multiLicenseText = gpl30 + "\n\n----------\n\n" + apache20;
 		String textWithRandomPrefixAndSuffix = "Some random preamble text.\n\n" + apache20 + "\n\nSome random epilogue text.";
-
+		String aladdin = ListedLicenses.getListedLicenses().getListedLicenseById("Aladdin").getLicenseText();
 		List<String> expectedResultEmpty = Arrays.asList();
 		List<String> expectedResultApache20 = Arrays.asList("Apache-2.0");
 		List<String> expectedResultGpl30 = Arrays.asList("GPL-3.0");
 		List<String> expectedResultGpl30Apache20 = Arrays.asList("GPL-3.0-only", "GPL-3.0", "Apache-2.0", "GPL-3.0-or-later", "GPL-3.0+");
-
+		List<String> expectedBsd2Clause = Arrays.asList("BSD-2-Clause");
+		List<String> expectedAladdin = Arrays.asList("Aladdin");
+		List<String> expectedSmlnj = Arrays.asList("SMLNJ");
 		// Note: be cautious about adding too many assertions to this test, as LicenseCompareHelper.matchingStandardLicenseIdsWithinText can have lengthy runtimes
 		assertListsEqual(expectedResultEmpty, LicenseCompareHelper.matchingStandardLicenseIdsWithinText(null));
 		assertListsEqual(expectedResultEmpty, LicenseCompareHelper.matchingStandardLicenseIdsWithinText(""));
-		assertListsEqual(expectedResultEmpty, LicenseCompareHelper.matchingStandardLicenseIdsWithinText("Some random text that isn't a standard license"));
-
-		assertListsEqual(expectedResultApache20, LicenseCompareHelper.matchingStandardLicenseIdsWithinText(apache20));
-		assertListsEqual(expectedResultGpl30Apache20, LicenseCompareHelper.matchingStandardLicenseIdsWithinText(multiLicenseText));
-		assertListsEqual(expectedResultApache20, LicenseCompareHelper.matchingStandardLicenseIdsWithinText(textWithRandomPrefixAndSuffix));
+		if (UnitTestHelper.runSlowTests()) {
+			assertListsEqual(expectedResultEmpty, LicenseCompareHelper.matchingStandardLicenseIdsWithinText("Some random text that isn't a standard license"));
+		}
 
 		// Tests for the 2-arg version of matchingStandardLicenseIdsWithinText (which is faster than the 1-arg version)
 		assertListsEqual(expectedResultApache20, LicenseCompareHelper.matchingStandardLicenseIdsWithinText(multiLicenseText, Arrays.asList("Apache-2.0")));
-		assertListsEqual(expectedResultGpl30, LicenseCompareHelper.matchingStandardLicenseIdsWithinText(multiLicenseText, Arrays.asList("GPL-3.0")));
+		if (UnitTestHelper.runSlowTests()) {
+			assertListsEqual(expectedResultGpl30, LicenseCompareHelper.matchingStandardLicenseIdsWithinText(multiLicenseText, Arrays.asList("GPL-3.0")));
+		}
+		// Test for all license IDs
+		if (UnitTestHelper.runSlowTests()) {
+			assertEquals(expectedAladdin, LicenseCompareHelper.matchingStandardLicenseIdsWithinText(aladdin, expectedAladdin));
+			assertEquals(expectedResultEmpty, LicenseCompareHelper.matchingStandardLicenseIdsWithinText(multiLicenseText, expectedAladdin));
+			assertEquals(expectedResultEmpty, LicenseCompareHelper.matchingStandardLicenseIdsWithinText(multiLicenseText, expectedSmlnj));
+			assertListsEqual(expectedResultApache20, LicenseCompareHelper.matchingStandardLicenseIdsWithinText(apache20));
+			assertListsEqual(expectedResultGpl30Apache20, LicenseCompareHelper.matchingStandardLicenseIdsWithinText(multiLicenseText));
+			assertListsEqual(expectedResultApache20, LicenseCompareHelper.matchingStandardLicenseIdsWithinText(textWithRandomPrefixAndSuffix));
+			assertListsEqual(expectedBsd2Clause, LicenseCompareHelper.matchingStandardLicenseIdsWithinText(UnitTestHelper.fileToText(ATLASSAIN_BSD_FILE), Arrays.asList("BSD-2-Clause")));
+		}
 	}
 
 	public void testMatchingStandardLicenseExceptionIdsWithinText() throws InvalidSPDXAnalysisException, SpdxCompareException, IOException {
@@ -805,12 +828,14 @@ public class LicenseCompareHelperTest extends TestCase {
 		assertListsEqual(expectedResultEmpty, LicenseCompareHelper.matchingStandardLicenseExceptionIdsWithinText(""));
 		assertListsEqual(expectedResultEmpty, LicenseCompareHelper.matchingStandardLicenseExceptionIdsWithinText("Some random text that isn't a standard license exception"));
 
-		assertListsEqual(expectedResultClasspathException20, LicenseCompareHelper.matchingStandardLicenseExceptionIdsWithinText(classpathException20));
-		assertListsEqual(expectedResultClasspathException20, LicenseCompareHelper.matchingStandardLicenseExceptionIdsWithinText(gpl20WithClasspathException20));
-		assertListsEqual(expectedResultClasspathException20, LicenseCompareHelper.matchingStandardLicenseExceptionIdsWithinText(textWithRandomPrefixAndSuffix));
-
 		// Tests for the 2-arg version of matchingStandardLicenseExceptionIdsWithinText (which is faster than the 1-arg version)
 		assertListsEqual(expectedResultClasspathException20, LicenseCompareHelper.matchingStandardLicenseExceptionIdsWithinText(gpl20WithClasspathException20, Arrays.asList("Classpath-exception-2.0")));
+
+		if (UnitTestHelper.runSlowTests()) {
+			assertListsEqual(expectedResultClasspathException20, LicenseCompareHelper.matchingStandardLicenseExceptionIdsWithinText(classpathException20));
+			assertListsEqual(expectedResultClasspathException20, LicenseCompareHelper.matchingStandardLicenseExceptionIdsWithinText(gpl20WithClasspathException20));
+			assertListsEqual(expectedResultClasspathException20, LicenseCompareHelper.matchingStandardLicenseExceptionIdsWithinText(textWithRandomPrefixAndSuffix));
+		}
 	}
 
 	public void testRegressionBSDProtection() throws InvalidSPDXAnalysisException, SpdxCompareException, IOException {
@@ -835,7 +860,7 @@ public class LicenseCompareHelperTest extends TestCase {
     
     public void testRegressionGD() throws InvalidSPDXAnalysisException, SpdxCompareException, IOException {
         String templateText = UnitTestHelper.fileToText(GD_TEMPLATE);
-        List<String> result = LicenseCompareHelper.getNonOptionalLicenseText(templateText, true);
+        List<String> result = LicenseCompareHelper.getNonOptionalLicenseText(templateText, VarTextHandling.ORIGINAL);
         assertEquals(1, result.size());
     }
     
@@ -880,5 +905,23 @@ public class LicenseCompareHelperTest extends TestCase {
         if (diff.isDifferenceFound()) {
         	fail(diff.getDifferenceMessage());
         }
+    }
+    
+    public void testNonOptionalTextToStartPattern() throws InvalidSPDXAnalysisException, SpdxCompareException {
+    	String expectedMatch = "This is line 1\nThis is line 2";
+    	List<String> noRegexes = Arrays.asList(new String[] {"This is line 1", "This is line 2"});
+    	assertTrue(LicenseCompareHelper.nonOptionalTextToStartPattern(noRegexes, 100).matcher(expectedMatch).matches());
+    	
+    	List<String> regexMiddle = Arrays.asList(new String[] {"This is~~~.+~~~1", "This is line 2"});
+    	assertTrue(LicenseCompareHelper.nonOptionalTextToStartPattern(regexMiddle, 100).matcher(expectedMatch).matches());
+    	
+    	List<String> regexStart = Arrays.asList(new String[] {"~~~.+~~~is line 1", "This is line 2"});
+    	assertTrue(LicenseCompareHelper.nonOptionalTextToStartPattern(regexStart, 100).matcher(expectedMatch).matches());
+    	
+    	List<String> regexEnd = Arrays.asList(new String[] {"This is line~~~.+~~~", "This is line 2"});
+    	assertTrue(LicenseCompareHelper.nonOptionalTextToStartPattern(regexEnd, 100).matcher(expectedMatch).matches());
+    	
+    	List<String> multipleRegex = Arrays.asList(new String[] {"~~~.+~~~is line~~~.+~~~", "This is line 2"});
+    	assertTrue(LicenseCompareHelper.nonOptionalTextToStartPattern(multipleRegex, 100).matcher(expectedMatch).matches());
     }
 }

TestFiles/atlassain-bsd
@@ -1,0 +1,23 @@
+Copyright (c) 2015, Atlassian Pty Ltd
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.

src/main/java/org/spdx/utility/compare/FilterTemplateOutputHandler.java
@@ -28,16 +28,32 @@ import org.spdx.licenseTemplate.LicenseTemplateRule;
  */
 public class FilterTemplateOutputHandler implements ILicenseTemplateOutputHandler {
 	
-	private boolean includeVarText;
+	public static final String REGEX_ESCAPE = "~~~";
+	public enum VarTextHandling {
+		OMIT,		// Omit the var text all together
+		ORIGINAL,	// Include the original text for the regex
+		REGEX,		// Include the regex itself included by the REGEX_ESCAPE strings
+	}
+	
+	private VarTextHandling varTextHandling;
 	private List<String> filteredText = new ArrayList<>();
 	StringBuilder currentString = new StringBuilder();
 	private int optionalDepth = 0;	// depth of optional rules
 
 	/**
-	 * @param includVarText if true, include the default variable text; if false remove the variable text
+	 * @param includeVarText if true, include the default variable text
 	 */
-	public FilterTemplateOutputHandler(boolean includVarText) {
-		this.includeVarText = includVarText;
+	@Deprecated
+	public FilterTemplateOutputHandler(boolean includeVarText) {
+		this(includeVarText ? VarTextHandling.ORIGINAL : VarTextHandling.OMIT);
+	}
+	
+	
+	/**
+	 * @param varTextHandling include original, exclude, or include the regex (enclosed with "~~~") for "var" text
+	 */
+	public FilterTemplateOutputHandler(VarTextHandling varTextHandling) {
+		this.varTextHandling = varTextHandling;
 	}
 
 	/* (non-Javadoc)
@@ -55,7 +71,11 @@ public class FilterTemplateOutputHandler implements ILicenseTemplateOutputHandle
 	 */
 	@Override
 	public void variableRule(LicenseTemplateRule rule) {
-		if (includeVarText && optionalDepth <= 0) {
+		if (VarTextHandling.REGEX.equals(varTextHandling) && optionalDepth <= 0) {
+			currentString.append(REGEX_ESCAPE);
+			currentString.append(rule.getMatch());
+			currentString.append(REGEX_ESCAPE);
+		} else if (VarTextHandling.ORIGINAL.equals(varTextHandling) && optionalDepth <= 0) {
 			currentString.append(rule.getOriginal());
 		} else {
 			if (currentString.length() > 0) {
@@ -100,7 +120,14 @@ public class FilterTemplateOutputHandler implements ILicenseTemplateOutputHandle
 	 * @return the includeVarText
 	 */
 	public boolean isIncludeVarText() {
-		return includeVarText;
+		return VarTextHandling.ORIGINAL.equals(varTextHandling);
+	}
+
+	/**
+	 * @return the varTextHandling
+	 */
+	public VarTextHandling getVarTextHandling() {
+		return varTextHandling;
 	}
 
 	/**

commit bcd8b2ee4d860d03c1d3eb04d1a3c414efe52ea9

Move license matching code which removes underlines
Fixes an issue with matching caused by PR #156

Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

src/main/java/org/spdx/utility/compare/LicenseCompareHelper.java
@@ -230,7 +230,6 @@ public class LicenseCompareHelper {
 	            	line = line.replaceAll("(\\*/|-->|-\\}|\\*\\))\\s*$", "");  // remove end of line comments
 	                line = line.replaceAll("^\\s*" + START_COMMENT_CHAR_PATTERN, "");  // remove start of line comments
                     line = line.replaceAll("^\\s*<<beginOptional>>\\s*" + START_COMMENT_CHAR_PATTERN, "<<beginOptional>>");
-                    line = line.replaceAll("(-|=|\\*){3,}", "");  // Remove ----, ***,  and ====
                     sb.append(line);
 	                sb.append("\n");
 	                line = reader.readLine();
@@ -940,6 +939,7 @@ public class LicenseCompareHelper {
 		Pattern matchPattern = nonOptionalTextToStartPattern(templateNonOptionalText, CROSS_REF_NUM_WORDS_MATCH);
 		List<Pair<Integer, Integer>> charPositions = new ArrayList<>();
 		String normalizedText = removeCommentChars(normalizeText(text));
+		normalizedText = normalizedText.replaceAll("(-|=|\\*){3,}", "");  // Remove ----, ***,  and ====
 		String compareText;
 		try {
 			compareText = normalizeTokensForRegex(normalizedText, charPositions);

src/test/java/org/spdx/utility/compare/LicenseCompareHelperTest.java
@@ -718,6 +718,7 @@ public class LicenseCompareHelperTest extends TestCase {
 		SpdxListedLicense gpl30 = ListedLicenses.getListedLicenses().getListedLicenseById("GPL-3.0");
 		SpdxListedLicense apache10 = ListedLicenses.getListedLicenses().getListedLicenseById("Apache-1.0");
 		SpdxListedLicense apache20 = ListedLicenses.getListedLicenses().getListedLicenseById("Apache-2.0");
+		SpdxListedLicense mplLicense = ListedLicenses.getListedLicenses().getListedLicenseById("MPL-2.0");
 		String multiLicenseText = gpl30.getLicenseText() + "\n\n----------\n\n" + apache20.getLicenseText();
 		String textWithRandomPrefixAndSuffix = "Some random preamble text.\n\n" + apache20.getLicenseText() + "\n\nSome random epilogue text.";
 
@@ -730,8 +731,9 @@ public class LicenseCompareHelperTest extends TestCase {
 		assertTrue(LicenseCompareHelper.isStandardLicenseWithinText(textWithRandomPrefixAndSuffix, apache20));
 		assertFalse(LicenseCompareHelper.isStandardLicenseWithinText(multiLicenseText, apache10));
 		String mplText = UnitTestHelper.fileToText(MPL_2_FROM_MOZILLA_FILE);
-		assertTrue(LicenseCompareHelper.isStandardLicenseWithinText(mplText, ListedLicenses.getListedLicenses().getListedLicenseById("MPL-2.0")));
-
+		assertTrue(LicenseCompareHelper.isStandardLicenseWithinText(mplText, mplLicense));
+		DifferenceDescription mplDiff = LicenseCompareHelper.isTextStandardLicense(mplLicense, mplText);
+		assertFalse(mplDiff.isDifferenceFound());
 
 /* Currently doesn't work - see https://github.com/spdx/Spdx-Java-Library/issues/141 for details
 		// JavaMail license is "CDDL-1.1 OR GPL-2.0 WITH Classpath-exception-2.0"