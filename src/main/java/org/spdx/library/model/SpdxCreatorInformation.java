a3cfc3824db3a5a33489d5d6b20d2aaffca45ebf
Gary O'Neall <gary@sourceauditor.com>
2024-08-19 14:42:27 -0700

    Allow x.y.z format in license list version
    Closes #247
    
    Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

--- src/main/java/org/spdx/library/model/SpdxCreatorInformation.java
+++ src/main/java/org/spdx/library/model/SpdxCreatorInformation.java
@@ -260,7 +260,7 @@ public class SpdxCreatorInformation extends ModelObject {
 		    if (SpdxConstants.LICENSE_LIST_VERSION_PATTERN.matcher(version).matches()) {
 		        return null;
 		    } else {
-		        return "License list version does not match the pattern M.N";
+		        return "License list version does not match the pattern M.N or M.N.P";
 		    }
 		}
 	}
