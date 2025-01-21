c930b026bce97c0a9aee148066861a8442c9025a
Gary O'Neall <gary@sourceauditor.com>
2025-01-21 14:42:06 -0800

    additional logging for licenses

--- src/main/java/org/spdx/storage/listedlicense/SpdxListedLicenseModelStore.java
+++ src/main/java/org/spdx/storage/listedlicense/SpdxListedLicenseModelStore.java
@@ -180,8 +180,10 @@ public abstract class SpdxListedLicenseModelStore implements IListedLicenseStore
                 ExceptionJsonTOC exceptionToc = gson.fromJson(tocJsonStr.toString(), ExceptionJsonTOC.class);
                 exceptionIds = exceptionToc.getExceptionIds();
             } catch (MalformedURLException e) {
+				logger.error("License TOC URL invalid", e);
 				throw new SpdxListedLicenseException("License TOC URL invalid", e) ;
 			} catch (IOException e) {
+				logger.error("I/O error reading license TOC", e);
 				throw new SpdxListedLicenseException("I/O error reading license TOC", e);
 			} finally {
             	if (reader != null) {
